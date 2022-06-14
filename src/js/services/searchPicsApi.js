f897bd4b233e4b2f5c28fc1eb64d843788c39e30
Alina Lemishko <aliska.nofear@gmail.com>
2022-06-14 10:29:14 +0300

    fixed url

--- src/js/services/searchPicsApi.js
+++ src/js/services/searchPicsApi.js
@@ -13,13 +13,20 @@ export default class SearchPicsService {
     this.page = 1;
     this.perPage = 40;
     this.totalPage = 0;
-
+    this.params = new URLSearchParams({
+      key: API_KEY,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: this.perPage,
+      page: this.page
+    })
   }
   
   async fetchPics() {
 
     try {
-      const response = await axios.get(`/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}`);
+      const response = await axios.get(`/?${this.params}&q=${this.searchQuery}`);
       const { hits, totalHits } = response.data;
       
       this.totalPage = Math.ceil(totalHits/this.perPage);
